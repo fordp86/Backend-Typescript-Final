54246e7a50f33aa9210ed72c9c0971ccb317147b
Ford Henley <fordp86@gmail.com>
2023-06-06 15:17:08 -0500

    Connected JawsDB
    Connected JawsDB

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -2,12 +2,12 @@ import { Sequelize } from "sequelize";
 import { RantFactory, AssociateUserRant } from "./rant";
 import { UserFactory } from "./user";
 
-const dbName = 'rantsDB';
-const username = 'root';
-const password = '123';
+const dbName = 'hoyrstdyzb3bsz4e';
+const username = 'exufwwlzdmqm48p8';
+const password = 'urbr9eik9qqxub7j';
 
 const sequelize = new Sequelize(dbName, username, password, {
-    host: 'localhost',
+    host: 'h1use0ulyws4lqr1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     port: 3306,
     dialect: 'mysql'
 });
